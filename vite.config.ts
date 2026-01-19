1f721c672814b79822bd1c230cc12a129ee4c763
honar mzory <yousifmzoryy@gmail.com>
2026-01-20 02:33:42 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
   // ✅ Add the base here
-  base: "mrovet-drawen",
+  base: "/mrovet-drawen",
 
   plugins: [
     react(), 
